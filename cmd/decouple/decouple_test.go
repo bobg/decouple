9bada1953b883232084eb157fb822a024690172d
Bob Glickstein <bobg@emphatic.com>
2024-01-21 09:56:30 -0800

    Add more coverage.

--- cmd/decouple/decouple_test.go
+++ cmd/decouple/decouple_test.go
@@ -5,10 +5,13 @@ import (
 	"encoding/json"
 	"path/filepath"
 	"reflect"
+	"strings"
 	"testing"
+
+	"github.com/bobg/go-generics/v3/iter"
 )
 
-func TestRun(t *testing.T) {
+func TestRunJSON(t *testing.T) {
 	buf := new(bytes.Buffer)
 	if err := run(buf, false, true, []string{"../.."}); err != nil {
 		t.Fatal(err)
@@ -42,5 +45,30 @@ func TestRun(t *testing.T) {
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestRunPlain(t *testing.T) {
+	buf := new(bytes.Buffer)
+	if err := run(buf, false, false, []string{"../.."}); err != nil {
+		t.Fatal(err)
+	}
+
+	lines, err := iter.ToSlice(iter.Lines(buf))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(lines) != 2 {
+		t.Fatalf("got %d lines, want 2", len(lines))
+	}
+	if !strings.HasSuffix(lines[0], ": showJSON") {
+		t.Fatalf(`line 1 is "%s", want something ending in ": showJSON"`, lines[0])
+	}
+
+	lines[1] = strings.TrimSpace(lines[1])
+	const want = "checker: [NameForMethods]"
+	if lines[1] != want {
+		t.Fatalf(`line 2 is "%s", want "%s"`, lines[1], want)
 	}
 }
